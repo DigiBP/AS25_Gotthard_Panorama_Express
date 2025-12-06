aa5f7b27ad123c8de830acf029924e64764c64b0
Djordji <djordji.pavloski@students.fhnw.ch>
2025-12-06 15:45:51 +0100

    Added an example to the checklist endpoint.

--- Application/backend/routers/checklists.py
+++ Application/backend/routers/checklists.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from typing import List
 from Application.backend.core.database import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from Application.backend.models.checklist import ChecklistItem, ChecklistItemResponse
+from Application.backend.models.checklist import ChecklistItem, ChecklistItemResponse, CHECKLIST_EXAMPLE
 from Application.backend.services.checklist_service import process_checklist
 
 router = APIRouter(prefix="/checklist", tags=["checklist"])
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/checklist", tags=["checklist"])
 
 @router.post("/", response_model=List[ChecklistItemResponse])
 async def evaluate_checklist(
-    items: List[ChecklistItem],
+    items: List[ChecklistItem] = Body(..., example=CHECKLIST_EXAMPLE),
     session: AsyncSession = Depends(get_session)
 ):
     return await process_checklist(items, session)
